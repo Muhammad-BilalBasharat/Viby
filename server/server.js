2896e123877e85e4358a4d337de82741cfc5e2be
Muhammad-BilalBasharat <mohammadbilalbasharat@gmail.com>
2025-10-03 17:01:12 +0500

    Viby Chat

--- server/server.js
+++ server/server.js
@@ -21,11 +21,17 @@ app.get('/', (req, res) => {
 // const app = express();
 app.use(express.json({limit: "50mb"}));
 app.use(cookieParser());
-app.use(cors(
-  {
-    origin:"https://viby-1-t7xs.onrender.com",
-    credentials: true,
-  }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://viby-1-t7xs.onrender.com",
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
+app.options("*", cors()); // handle preflight
+
 
 const PORT = process.env.PORT || 5000;
 
